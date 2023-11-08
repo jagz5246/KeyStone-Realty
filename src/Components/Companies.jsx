f07da7559443cb6dc2389727e71cabbd7e9492aa
Jagan G Mohan <59693136+jagz5246@users.noreply.github.com>
2023-11-08 15:49:40 +0530

    Update Companies.jsx

--- src/Components/Companies.jsx
+++ src/Components/Companies.jsx
@@ -24,37 +24,7 @@ const Companies = () => {
 
   return (
     <Box sx={{ mt: 10 }}>
-      <CustomContainer>
-        <CustomBox>
-          <img src={logoImg} alt="logo" style={{ maxWidth: "100%" }} />
-          <Typography
-            variant="body2"
-            sx={{
-              color: "#7D8589",
-              fontSize: "16px",
-              fontWeight: "bold",
-              mt: 2,
-            }}
-          >
-            More than 45,000 trust Keystone
-          </Typography>
-        </CustomBox>
-
-        <Box>
-          <img src={starsImg} alt="stars" style={{ maxWidth: "100%" }} />
-          <Typography
-            variant="body2"
-            sx={{
-              color: "#7D8589",
-              fontSize: "16px",
-              fontWeight: "bold",
-              mt: 2,
-            }}
-          >
-            5-Star Rating (2k+ Reviews)
-          </Typography>
-        </Box>
-      </CustomContainer>
+     
 
       <Container sx={{ display: "flex", flexDirection: "column" }}>
         <img src={logosImg} alt="logos" />
