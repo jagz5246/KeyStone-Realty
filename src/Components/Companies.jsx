65c257120530b4c9c8e33dbc1d4d0e99bec8e8b1
Jagan G Mohan <59693136+jagz5246@users.noreply.github.com>
2023-11-14 23:44:18 +0530

    Update Companies.jsx

--- src/Components/Companies.jsx
+++ src/Components/Companies.jsx
@@ -1,48 +1,48 @@
-import { Box, Container, styled, Typography } from "@mui/material";
-import React from "react";
-import logosImg from "../media/logos.png";
+// import { Box, Container, styled, Typography } from "@mui/material";
+// import React from "react";
+// import logosImg from "../media/logos.png";
 
-const Companies = () => {
-  const CustomContainer = styled(Container)(({ theme }) => ({
-    display: "flex",
-    justifyContent: "center",
-    [theme.breakpoints.down("md")]: {
-      flexDirection: "column",
-      alignItems: "center",
-      textAlign: "center",
-      marginBottom: theme.spacing(4)
-    },
-  }));
+// const Companies = () => {
+//   const CustomContainer = styled(Container)(({ theme }) => ({
+//     display: "flex",
+//     justifyContent: "center",
+//     [theme.breakpoints.down("md")]: {
+//       flexDirection: "column",
+//       alignItems: "center",
+//       textAlign: "center",
+//       marginBottom: theme.spacing(4)
+//     },
+//   }));
 
-  const CustomBox = styled(Box)(({ theme }) => ({
-    [theme.breakpoints.down("md")]: {
-      marginBottom: theme.spacing(4),
-    },
-  }));
+//   const CustomBox = styled(Box)(({ theme }) => ({
+//     [theme.breakpoints.down("md")]: {
+//       marginBottom: theme.spacing(4),
+//     },
+//   }));
 
-  return (
-    <Box sx={{ mt: 10 }}>
-      <CustomContainer>
-        <CustomBox>
-          <Typography
-            variant="body2"
-            sx={{
-              color: "#7D8589",
-              fontSize: "24px",
-              fontWeight: "bold",
-              mt: 2,
-            }}
-          >
-            More than 45,000 trust Keystone realty
-          </Typography>
-        </CustomBox>
-      </CustomContainer>
+//   return (
+//     <Box sx={{ mt: 10 }}>
+//       <CustomContainer>
+//         <CustomBox>
+//           <Typography
+//             variant="body2"
+//             sx={{
+//               color: "#7D8589",
+//               fontSize: "24px",
+//               fontWeight: "bold",
+//               mt: 2,
+//             }}
+//           >
+//             More than 45,000 trust Keystone realty
+//           </Typography>
+//         </CustomBox>
+//       </CustomContainer>
 
-      <Container sx={{ display: "flex", flexDirection: "column" }}>
-        <img src={logosImg} alt="logos" />
-      </Container>
-    </Box>
-  );
-};
+//       <Container sx={{ display: "flex", flexDirection: "column" }}>
+//         <img src={logosImg} alt="logos" />
+//       </Container>
+//     </Box>
+//   );
+// };
 
-export default Companies;
+// export default Companies;
